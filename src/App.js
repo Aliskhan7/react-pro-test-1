3dfe61ef89f07b5dc07b07e69721bbd7381340fc
juleybib <aliskhangazamatov@gmail.com>
2021-04-26 14:04:39 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    const [data, setData] = useState([])
+    const [load, setLoad] = useState(true)
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then ((res) => {
+        return res.json();
+        })
+        .then((json) => {
+            setData (json);
+            setLoad(false)
+        });
+
+    useSelector(state => state);
+
+
+
+
+    return (
+      <div>
+          список дел:
+          {load ? (
+              <div>
+                  loading...
+              </div>
+          ) : (
+              data.map(item => {
+                  return(
+                      <div>
+                          {item.title}
+                      </div>
+                  )
+              })
+          ) }
+      </div>
   );
 }
 
